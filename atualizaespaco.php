61938c0d9bf5dc9fbb57ec84fd1fe92c65622178
Matheuzzzzzzz <124745048+Matheuzzzzzzz@users.noreply.github.com>
2024-06-20 13:58:06 -0300

    Update atualizaespaco.php

--- atualizaespaco.php
+++ atualizaespaco.php
@@ -45,7 +45,7 @@
                 </div>
             </li>
             <li>
-                <a href=""><svg id="svg02" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <a href="Login.php"><svg id="svg02" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
                     </svg></a>
             </li>
@@ -53,7 +53,6 @@
 
     </nav>
 </header>
-<div class="main"></div>
 <div id="meio">
     <div id="caixacentral">
         <div id="CadastroCliente"> Atualizar dados</div>
