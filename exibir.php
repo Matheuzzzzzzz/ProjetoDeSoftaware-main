2381f0fd926df7ec31581a935a84fb1e457dc7c2
DaviLeo <119076543+Davizn-Leo@users.noreply.github.com>
2024-08-01 14:44:44 -0300

    Update exibir.php

--- exibir.php
+++ exibir.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,9 +9,10 @@
     <link rel="stylesheet" href="homecss.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
     <header>
-    <nav>
+        <nav>
             <ul class="listnav">
                 <li>
                     <div class="dropdown">
@@ -32,10 +34,10 @@
                 </li>
 
                 <li>
-                    <div class="search-container">
-                        <input type="text" id="search-input" class="search-input" placeholder="Digite sua pesquisa...">
-                        <button class="search-button" onclick="search()">Pesquisar</button>
-                    </div>
+                    <form class="search-container" action="busca.php" method="get">
+                        <input type="text" name="busca" id="search-input" class="search-input" placeholder="Digite sua pesquisa...">
+                        <button type="submit">Pesquisar</button>
+                    </form>
                 </li>
                 <li>
                     <a href="Login.php"><svg id="svg02" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
@@ -48,88 +50,23 @@
 
     </header>
 
-    <h1 class="localtxt"> Locais </h1>
-    <div class="produtos">
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    </div>
-<footer class="pe">
-<div id="borda">
-        <div class="promo">
-            <h3>Cadastre seu espaço para garantir clientes!</h3>
-            
-            <div>
-            <img src="https://images.vexels.com/media/users/3/205157/isolated/preview/3a1d0d70d14ba68cd27271c889618e51-ilustracao-do-espaco-astronauta.png" alt="">   <a href="">
-                Acesse Aqui
-            </a>
+    <h1 class="localtxt"> Local </h1>
+
+    <footer class="pe">
+        <div id="borda">
+            <div class="promo">
+                <h3>Cadastre seu espaço para garantir clientes!</h3>
+
+                <div>
+                    <img src="https://images.vexels.com/media/users/3/205157/isolated/preview/3a1d0d70d14ba68cd27271c889618e51-ilustracao-do-espaco-astronauta.png" alt=""> <a href="">
+                        Acesse Aqui
+                    </a>
+                </div>
             </div>
-        </div>
-        <button id="btnentrar">Entrar</button>
+            <button id="btnentrar">Entrar</button>
             <button id="btncadastrov">Cadastra-se</button>
         </div>
-        
+
         <ul id="ulfooter">
             <li>All Access
                 <ul class="listbelow">
@@ -164,7 +101,115 @@
             </li>
         </ul>
 
-</footer>
+    </footer>
 
 </body>
+
+</html><!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Locais</title>
+    <link rel="stylesheet" href="exibircss.css">
+    <link rel="stylesheet" href="homecss.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul class="listnav">
+                <li>
+                    <div class="dropdown">
+                        <a href="">
+                            <svg id="svg01" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                            </svg>
+                        </a>
+                        <ul class="droplist">
+                            <li><a href="">Produtos Festivos</a></li>
+                            <li><a href="cadastroespaco.php">Espaços Festivos</a></li>
+                            <li><a href="cadastrocliente.php">Cadastro Cliente</a></li>
+                            <li><a href="">Cadastro de Lojas/Vendas</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <h1><img id="astronalta" src="https://images.vexels.com/media/users/3/205157/isolated/preview/3a1d0d70d14ba68cd27271c889618e51-ilustracao-do-espaco-astronauta.png" alt=""><a href="index.php">Planeta Festival</a></h1>
+                </li>
+
+                <li>
+                    <form class="search-container" action="busca.php" method="get">
+                        <input type="text" name="busca" id="search-input" class="search-input" placeholder="Digite sua pesquisa...">
+                        <button type="submit">Pesquisar</button>
+                    </form>
+                </li>
+                <li>
+                    <a href="Login.php"><svg id="svg02" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                        </svg></a>
+                </li>
+            </ul>
+
+        </nav>
+
+    </header>
+
+    <h1 class="localtxt"> Local </h1>
+
+    <footer class="pe">
+        <div id="borda">
+            <div class="promo">
+                <h3>Cadastre seu espaço para garantir clientes!</h3>
+
+                <div>
+                    <img src="https://images.vexels.com/media/users/3/205157/isolated/preview/3a1d0d70d14ba68cd27271c889618e51-ilustracao-do-espaco-astronauta.png" alt=""> <a href="">
+                        Acesse Aqui
+                    </a>
+                </div>
+            </div>
+            <button id="btnentrar">Entrar</button>
+            <button id="btncadastrov">Cadastra-se</button>
+        </div>
+
+        <ul id="ulfooter">
+            <li>All Access
+                <ul class="listbelow">
+                    <li>Seu plano ideal</li>
+                </ul>
+            </li>
+            <li>
+                Planeta Festival
+                <ul class="listbelow">
+                    <li>Conheça</li>
+                    <li>Carreiras</li>
+                    <li>Sala de Imprensa</li>
+                    <li>Eventos</li>
+                </ul>
+            </li>
+            <li>Atendimento
+                <ul class="listbelow">
+                    <li></li>
+                    <li>Entre em contato</li>
+                    <li>Perguntas frequentes</li>
+                </ul>
+            </li>
+            <li>INFORMAÇÕES
+                <ul class="listbelow">
+                    <li>Termos de Serviço</li>
+                    <li>Política de Privacidade</li>
+                    <li>Política de Cookies</li>
+                    <li>Regulamentos de Promoções</li>
+                    <li>Regras de Convivências Membros WeWork</li>
+                    <li>Código de Conduta Fornecedores</li>
+                </ul>
+            </li>
+        </ul>
+
+    </footer>
+
+</body>
+
 </html>
